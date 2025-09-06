5a1f7c573473366dc408b953af714be6609ebc10
pavan-ak1 <kustagipavan30@gmail.com>
2025-09-06 10:04:43 +0530

    Pushed init and user authentication

---
+++ backend/index.js
@@ -0,0 +1,66 @@
+require("dotenv").config();
+require('express-async-errors');
+
+const express = require("express");
+const app = express();
+const morgan = require('morgan');
+const cookieParser = require('cookie-parser');
+const cors = require('cors')
+const fileUpload = require('express-fileupload')
+const rateLImiter  =require('express-mongo-sanitize');
+const helmet = require('helmet');
+const xss = require('xss-clean');
+const mongoSanitize = require('express-mongo-sanitize')
+
+
+// Database Connection
+const connectDb = require("./db/connectDb");
+
+// Routers
+const authRoutes = require('./routes/authRoutes');
+
+// Middleware
+const notFoundMiddleware = require('./middleware/not-found');
+const errorHandlerMiddleware = require('./middleware/error-handler');
+
+
+app.set('trust proxy', 1);
+app.use(rateLImiter({
+  windowMs:15*0*1000,
+  max:60,
+}));
+app.use(helmet())
+app.use(xss());
+app.use(mongoSanitize())
+app.use(express.json());
+app.use(cookieParser(process.env.JWT_SECRET));
+app.use(express.static('./public'));
+app.use(fileUpload())
+app.use(cors());
+
+
+
+
+
+//routes
+app.use('/api/v1/auth', authRoutes);
+
+
+//middleware
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
+
+const port = process.env.PORT || 5000;
+
+const start = async () => {
+  try {
+    await connectDb(process.env.MONGO_URI);
+    app.listen(port, () => {
+      console.log(`Server running on port ${port}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
